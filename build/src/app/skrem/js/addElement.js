f7e9aee6d714891f8474e0fd65c9c5ce3e9a6d9f
alejs-dev <alexmardarilel@gmail.com>
2025-02-14 23:35:28 +0100

    ka bearbeiten von stuff ich weiss es nicht

--- build/src/app/skrem/js/addElement.js
+++ build/src/app/skrem/js/addElement.js
@@ -1,4 +1,18 @@
+const sql = require("sqlite3").verbose();
+const database = new sql.Database("../db/skrem.sqlite3");
+    
+const insertQuery = `INSERT INTO skrem (producer, name) VALUES (?, ?)`;
+
 function submit() {
+
     let inputProducer = document.getElementById("inputProducer").value;
     let inputName = document.getElementById("inputName").value;
+
+    database.run(insertQuery, [inputProducer, inputName], function (err) {
+        if (err) {
+            console.error(err.message);
+        } else {
+            console.log("Inserted Data");
+        }
+    });
 }
